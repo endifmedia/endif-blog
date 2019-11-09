018861fb9535620e8836da5cd90f1c6073af9165
Ethan Allen <ethan@getstringcan.com>
2019-11-09 11:02:01 -0500

    add user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
   has_many :posts
   before_save { self.email = email.downcase }
 
@@ -8,5 +7,7 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: {case_sensitive: false},
   			length: {maximum: 150},
-  			format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+			  format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+			  
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_143523) do
+ActiveRecord::Schema.define(version: 2019_11_09_154330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_11_09_143523) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
